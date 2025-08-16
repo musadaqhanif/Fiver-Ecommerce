c1eef704295857d6cfa75ac81cab16cd6312d3e2
Muhammad Hassan Iqbal <hassanawan7349@gmail.com>
2025-08-16 23:41:06 +0500

    temp

--- src/app/chat-support/page.tsx
+++ src/app/chat-support/page.tsx
@@ -1,6 +1,6 @@
 import SupportChatInbox from "@/imports/SupportChatInbox-1-1139";
 
-export default function App() {
+export default function Page() {
   return (
     <div className="size-full">
       <SupportChatInbox />
